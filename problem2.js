eeda23925a883e4ceec464bbdd11358275696888
Guillaume Ch <cguille.dev@gmail.com>
2013-10-31 19:56:14 +0100

    Problem 2

---
+++ problem2.js
@@ -0,0 +1,61 @@
+(function () {
+    'use strict';
+
+    /**
+     * Extends the Number prototype, providing a method to check whether the
+     * the number is a multiple of the given number or not
+     * @param  {number}  multiple
+     * @return {Boolean}
+     */
+    Number.prototype.isMultipleOf = function isMultipleOf(multiple) {
+        return (this % multiple) === 0;
+    };
+
+    var argv = process.argv.slice(2),
+        argc = argv.length;
+
+    if (argc !== 1) {
+        console.error('Error: this command needs 1 argument.');
+        console.error('Usage: node problem2.js <limit>');
+        process.exit(1);
+    }
+
+    var limit = +(argv.pop());
+
+    console.log(sumOfEvenFibonacciBelow(limit));
+
+    /**
+     * Compute the sum of the fibonacci numbers that are even and below the
+     * given limit
+     * @param  {number} limit
+     * @return {number}
+     */
+    function sumOfEvenFibonacciBelow(limit) {
+        var sum = 0;
+
+        fibonacci(function (value) {
+            if (value.isMultipleOf(2)) {
+                sum += value;
+            }
+            return value <= limit;
+        });
+
+        return sum;
+    }
+
+    /**
+     * Feed the callback with fibonacci numbers while it returns true
+     * @param  {Function} callback
+     */
+    function fibonacci(callback) {
+        var beforePrevious = 0,
+            previous = 1,
+            current = beforePrevious + previous;
+
+        while (true === callback(current)) {
+            beforePrevious = previous;
+            previous = current;
+            current = beforePrevious + previous;
+        }
+    }
+}());
